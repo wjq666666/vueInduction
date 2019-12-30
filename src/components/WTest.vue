2cfc61db1a34affd34bda66e5f8e553e48b99744
wjq666666 <1550763637@qq.com>
2019-12-30 18:25:39 +0800

    master

---
+++ src/components/WTest.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div>元旦放假时间为：{{time}}</div>
+    <div>{{ $attrs.title }}</div>
+    <div>{{ msg }}</div>
+    <div>地址：{{address}}</div>
+    <div>
+      <slot name="title"></slot>
+    </div>
+    <div>
+      <slot name="content" :age="age"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WTest",
+  props: {
+    msg: String
+  },
+  inject: ['address'],
+  data() {
+    return{
+      time: '2020年1月1日',
+      count: 1,
+      age: 28,
+      myName: '',
+    }
+  },
+  mounted() {
+    this.$parent.$emit('WT', this.age);
+    //this.$emit('change', this.count);
+    /* this.$parent.$on('HC', (name) => {
+      console.log('监听HC', name);
+      this.myName = name;
+    }); */
+    
+  }
+};
+</script>
+
+<style></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WTest title="关于春节放假通知" :msg="msg" ref="test">
+      <template v-slot:title>标题</template>
+      <template v-slot:content="{age}">内容...{{age}}</template>
+    </WTest>
+    <!-- <div>{{content}}</div> -->
   </div>
 </template>
+<script>
+import WTest from "../components/WTest";
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  name: "About",
+  components: {
+    WTest,
+    HelloWorld
+  },
+  provide() {
+    return {
+      address: '北京市西城区'
+    }
+  },
+  data() {
+    return {
+      msg: "2019年12月30日",
+      content: '',
+    };
+  },
+  mounted() {
+    this.$refs.test.time = '2019年12月31日下午五点';
+    this.msg = '2020年1月17日 - 2020年2月2日';
+    this.$children[0].time = "2019年12月31日下午七点";
+  },
+  methods: {
+    /* timeChange(a) {
+      this.content = ++a;
+      console.log(a);
+    } */
+  },
+};
+</script>
