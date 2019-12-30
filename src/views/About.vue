cd3740c0fc1ce8038f3a797d02240741a14efe93
wjq666666 <1550763637@qq.com>
2019-12-30 13:51:18 +0800

    wjq/test2

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <div>再过两天就要2020年</div>
-    <div>鼠年大吉</div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,6 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>This is home page</div>
-    <div>再过两天就要2020年</div>
-    <div>鼠年大吉</div>
   </div>
 </template>
 
