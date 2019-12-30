50e254904fcb1e7e759343fd50d772f4e8435396
wjq666666 <1550763637@qq.com>
2019-12-30 11:23:34 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>This is home page</div>
   </div>
 </template>
 
