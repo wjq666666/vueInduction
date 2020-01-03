6ae14430f1ad7739b3c6246b44f8491c3f85b309
wjq666666 <1550763637@qq.com>
2020-01-03 10:33:42 +0800

    aa

--- src/components/form/WInput.vue
+++ src/components/form/WInput.vue
@@ -1,32 +1,30 @@
 <template>
   <div>
-      <input :type="type" :value="value" @input="oninput" v-bind="$attrs">
+    <input :type="type" :value="value" @input="oninput" v-bind="$attrs" />
   </div>
 </template>
 
 <script>
 export default {
-    props: {
-        value: {
-            type: String,
-            default: ''
-        },
-        type: {
-            type: String,
-            default: 'text'
-        }
+  props: {
+    value: {
+      type: String,
+      default: ""
     },
-    methods: {
-        oninput(e) {
-           this.$emit('input',e.target.value);
-        //    this.$emit('update:value',e.target.value);
-            // 通知校验
-            this.$parent.$emit('validate');
-        }
-    },
-}
+    type: {
+      type: String,
+      default: "text"
+    }
+  },
+  methods: {
+    oninput(e) {
+      this.$emit("input", e.target.value);
+      //    this.$emit('update:value',e.target.value);
+      // 通知校验
+      this.$parent.$emit("validate");
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
